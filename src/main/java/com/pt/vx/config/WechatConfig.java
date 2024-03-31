734ac136ccb3ed70c66645919a5a44cd38ff44c9
riverchina <33343185+riverchina@users.noreply.github.com>
2024-04-01 00:48:33 +0800

    Update WechatConfig.java
    1

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -38,10 +38,10 @@ public class WechatConfig {
      */
     static {
         userList.add(getUser(
-                "这个人扫码后的微信号", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID", //要给这个人发送的模板ID
-                "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
+                "obQ1c6_zlYpNqZkHv31Gqzp0oBiY", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "Tq-GdH8RRzLLhQLXUg0s_iEqYEa_tSPi-aaoA3rwRs4", //要给这个人发送的模板ID
+                "何先生", //咋称呼这个人
+                "江苏省南京市浦口", //这个人的详细地址
                 "南京", //这个人在的城市
                 new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
                 new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
@@ -49,17 +49,17 @@ public class WechatConfig {
                 new BirthDay(2020,7,8,true,false,"周年快乐！！！")
         ));
 
-        userList.add(getUser(
-                "这个人扫码后的微信号",
-                "微信消息模板ID",
-                "这个人的称呼",
-                "江苏省南京市玄武区",
-                "南京",
-                new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
-                new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false,"周年快乐！！！")
-        ));
+        // userList.add(getUser(
+        //         "这个人扫码后的微信号",
+        //         "微信消息模板ID",
+        //         "这个人的称呼",
+        //         "江苏省南京市玄武区",
+        //         "南京",
+        //         new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
+        //         new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
+        //         new BirthDay(2020,7,8,true,true),
+        //         new BirthDay(2020,7,8,true,false,"周年快乐！！！")
+        // ));
 
 
     }
